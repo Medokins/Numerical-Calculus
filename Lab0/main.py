76b5c6e320b584f79dfeff6990ab77375eb9e159
Medokins <nikodemszwast@gmail.com>
2022-03-09 12:31:31 +0100

    add update

--- Lab0/main.py
+++ Lab0/main.py
@@ -1,7 +1,6 @@
 import numpy as np
 
-rows = 5
-columns = 5
+size = 5
 verbose = False
 
 GausianElimantion = False
@@ -9,7 +8,7 @@ GausianJordanElimination = True
 
 if GausianElimantion:
     for q in np.arange(0.2, 1.0, 0.15):
-        solution = np.zeros(rows)
+        solution = np.zeros(size)
 
         matrix_A = [
                     [2*q*pow(10, -4), 1, 6, 9, 10, 10],
@@ -20,12 +19,12 @@ if GausianElimantion:
                     ]
 
         #Eliminacja Gaussa:
-        for i in range(rows):
+        for i in range(size):
 
-            for j in range(i+1, columns):
+            for j in range(i+1, size):
                 l = matrix_A[j][i]/matrix_A[i][i]
 
-                for k in range(columns + 1):
+                for k in range(size + 1):
                     matrix_A[j][k] = round(matrix_A[j][k] - l * matrix_A[i][k], 3)
 
 
@@ -36,12 +35,12 @@ if GausianElimantion:
 
         #Cofanie się od ostatniego rzędu i wylia czanie wyników:
         #Wyliczam pierwszy "na sztywno" żeby móc potem z niego korzystać
-        solution[rows-1] = matrix_A[rows-1][columns]/matrix_A[rows-1][rows-1]
+        solution[size-1] = matrix_A[size-1][size]/matrix_A[size-1][size-1]
 
-        for i in range(rows-2,-1,-1):
-            solution[i] = matrix_A[i][rows]
+        for i in range(size-2,-1,-1):
+            solution[i] = matrix_A[i][size]
             
-            for j in range(i+1,rows):
+            for j in range(i+1,size):
                 solution[i] = solution[i] - matrix_A[i][j]*solution[j]
             
             solution[i] = solution[i]/matrix_A[i][i]
@@ -50,7 +49,7 @@ if GausianElimantion:
 
 if GausianJordanElimination:
     for q in np.arange(0.2, 5.0, 0.15):
-        solution = np.zeros(rows)
+        solution = np.zeros(size)
 
         matrix_A = [
                     [2*q*pow(10, -4), 1, 6, 9, 10, 10],
@@ -60,22 +59,22 @@ if GausianJordanElimination:
                     [3,4,9,7,9, 3]
                     ]
 
-        for i in range(rows):
+        for i in range(size):
 
             temp1 = matrix_A[i][i]
 
-            for k in range(rows + 1):
+            for k in range(size + 1):
                 matrix_A[i][k] =matrix_A[i][k] / temp1
 
-            for j in range(rows):
+            for j in range(size):
                 if i != j:
                     temp2= matrix_A[j][i]
 
-                    for k in range(rows+1):
+                    for k in range(size+1):
                         matrix_A[j][k] = round(matrix_A[j][k] - temp2 * matrix_A[i][k], 5)
 
 
-        for i in range(rows):
-            solution[i] = matrix_A[i][rows]/matrix_A[i][i]
+        for i in range(size):
+            solution[i] = matrix_A[i][size]/matrix_A[i][i]
 
         print(solution)
