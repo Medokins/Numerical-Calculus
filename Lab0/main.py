b3ee3a1bda0e22e5cc0e7b74a9d64f865304ff21
Medokins <nikodemszwast@gmail.com>
2022-03-09 19:06:55 +0100

    add graphs and deviation

--- Lab0/main.py
+++ Lab0/main.py
@@ -1,14 +1,29 @@
 import numpy as np
+import math
+import matplotlib.pyplot as plt
 
 size = 5
 verbose = False
 
-GausianElimantion = False
+GausianElimantion = True
 GausianJordanElimination = True
 
 if GausianElimantion:
-    for q in np.arange(0.2, 1.0, 0.15):
+    print(f"Gaussian Elimination\n")
+    deviations = []
+    qs = []
+
+    for q in np.arange(0.2, 5.0, 0.15):
+        q = round(q, 2)
         solution = np.zeros(size)
+
+        backup_matrix = [
+                    [2*q*pow(10, -4), 1, 6, 9, 10, 10],
+                    [2*pow(10, -4), 1, 6, 9, 10, 2],
+                    [1,6,6,8,6,9],
+                    [5,9,10,7,10, 9],
+                    [3,4,9,7,9, 3]
+                        ]
 
         matrix_A = [
                     [2*q*pow(10, -4), 1, 6, 9, 10, 10],
@@ -18,14 +33,12 @@ if GausianElimantion:
                     [3,4,9,7,9, 3]
                     ]
 
-        #Eliminacja Gaussa:
+        #Gausian Elimantion:
         for i in range(size):
-
             for j in range(i+1, size):
                 l = matrix_A[j][i]/matrix_A[i][i]
-
-                for k in range(size + 1):
-                    matrix_A[j][k] = round(matrix_A[j][k] - l * matrix_A[i][k], 3)
+                for k in range(0, size + 1):
+                    matrix_A[j][k] = matrix_A[j][k] - l * matrix_A[i][k]
 
 
         if verbose:
@@ -33,23 +46,49 @@ if GausianElimantion:
                 print(f"{row}")
             print("\n", "#" * 40, "\n")
 
-        #Cofanie się od ostatniego rzędu i wylia czanie wyników:
-        #Wyliczam pierwszy "na sztywno" żeby móc potem z niego korzystać
-        solution[size-1] = matrix_A[size-1][size]/matrix_A[size-1][size-1]
+
+        solution[size-1] = matrix_A[size-1][size] / matrix_A[size-1][size-1]
 
         for i in range(size-2,-1,-1):
             solution[i] = matrix_A[i][size]
-            
             for j in range(i+1,size):
                 solution[i] = solution[i] - matrix_A[i][j]*solution[j]
-            
-            solution[i] = solution[i]/matrix_A[i][i]
+            solution[i] = solution[i] / matrix_A[i][i]
 
-        print(solution)
+        sum = 0
+        for i in range(size):
+            c = 0
+            for j in range(size):
+                c += backup_matrix[i][j] * solution[j]
+            sum += math.pow(c - backup_matrix[i][size], 2) 
+        deviation = math.sqrt(sum) / size 
+
+        qs.append(q)
+        deviations.append(deviation)
+        print(f"For q = {q} deviation o(q) = {deviation}")
+
+    plt.plot(qs, deviations)
+    plt.xlabel("q")
+    plt.ylabel("deviation")
+    plt.show()
 
 if GausianJordanElimination:
+    print(f"\nGaussian-Jordan Elimination\n")
+    deviations = []
+    qs = []
+
+
     for q in np.arange(0.2, 5.0, 0.15):
+        q = round(q, 2)
         solution = np.zeros(size)
+
+        backup_matrix = [
+                    [2*q*pow(10, -4), 1, 6, 9, 10, 10],
+                    [2*pow(10, -4), 1, 6, 9, 10, 2],
+                    [1,6,6,8,6,9],
+                    [5,9,10,7,10, 9],
+                    [3,4,9,7,9, 3]
+                        ]
 
         matrix_A = [
                     [2*q*pow(10, -4), 1, 6, 9, 10, 10],
@@ -71,10 +110,26 @@ if GausianJordanElimination:
                     temp2= matrix_A[j][i]
 
                     for k in range(size+1):
-                        matrix_A[j][k] = round(matrix_A[j][k] - temp2 * matrix_A[i][k], 5)
+                        matrix_A[j][k] = matrix_A[j][k] - temp2 * matrix_A[i][k]
 
 
         for i in range(size):
             solution[i] = matrix_A[i][size]/matrix_A[i][i]
 
-        print(solution)
+        sum = 0
+        for i in range(size):
+            c = 0
+            for j in range(size):
+                c += backup_matrix[i][j] * solution[j]
+            sum += math.pow(c - backup_matrix[i][5], 2)
+
+        deviation = math.sqrt(sum) / size 
+
+        qs.append(q)
+        deviations.append(deviation)
+        print(f"For q = {q} deviation o(q) = {deviation}")
+
+    plt.plot(qs, deviations)
+    plt.xlabel("q")
+    plt.ylabel("deviation")
+    plt.show()
